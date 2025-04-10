d5c05951acc4d7688cbedd284b8b168153419739
Aimar Ibarra <aimar_ibarra@proton.me>
2025-04-10 11:48:50 +0200

    Cleanup

--- app/src/test/java/io/github/costsplit/app/AppTest.java
+++ app/src/test/java/io/github/costsplit/app/AppTest.java
@@ -1,14 +1,20 @@
 package io.github.costsplit.app;
 
 import io.github.costsplit.api.Request;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.jackson.JacksonConverterFactory;
 import retrofit2.http.Body;
 import retrofit2.http.POST;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AppTest {
     App app;
@@ -41,12 +47,15 @@ public class AppTest {
     }
 
     @Test
-    void createUser() {
+    void createUser() throws IOException {
         var call = retrofit.create(AppService.class)
                 .createUser(new Request.CreateUser("receiver", "receiver@test.net", "p455w0rd"));
         var response = assertDoesNotThrow(call::execute);
-        var callWasCorrect = response.isSuccessful() || response.code() == 500;
-        assertTrue(callWasCorrect);
+        if (response.isSuccessful())
+            return;
+        try (var errbody = response.errorBody()) {
+            assertEquals("Couldn't send confirmation mail", new String(errbody.bytes(), StandardCharsets.UTF_8));
+        }
     }
 
     public interface AppService {
