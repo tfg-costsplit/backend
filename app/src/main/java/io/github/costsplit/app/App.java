fa91ce3844d27dbef7625be557bf0f98df055b34
Aimar Ibarra <aimar_ibarra@proton.me>
2025-04-10 11:27:38 +0200

    Add first test

--- app/src/main/java/io/github/costsplit/app/App.java
+++ app/src/main/java/io/github/costsplit/app/App.java
@@ -7,8 +7,15 @@ import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import io.github.costsplit.api.Request;
 import io.javalin.Javalin;
+import io.javalin.http.BadRequestResponse;
 import io.javalin.http.ForbiddenResponse;
+import io.javalin.http.InternalServerErrorResponse;
 import io.javalin.http.UnauthorizedResponse;
+import lombok.Builder;
+import lombok.Getter;
+import org.apache.commons.mail.DefaultAuthenticator;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
 
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
@@ -19,13 +26,36 @@ import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.Base64;
 
-public class App {
+import static java.lang.System.getenv;
 
+public class App {
     private static final SecureRandom RANDOM = new SecureRandom();
-    private static final String DOMAIN = "localhost";
-    private static final String SECRET = System.getenv("COSTSPLIT_SECRET");
-    private static final Algorithm algorithm = Algorithm.HMAC256(SECRET);
-    private static final JWTVerifier verifier = JWT.require(algorithm).build();
+    private final Algorithm algorithm;
+    private final JWTVerifier verifier;
+
+    @Getter
+    private final Config config;
+    private Javalin javalin;
+
+    private App(Config config) {
+        this.config = config;
+        algorithm = Algorithm.HMAC256(config.secret);
+        verifier = JWT.require(algorithm).build();
+    }
+
+    public static void main(String[] args) {
+        Config.builder()
+                .secret(getenv("COSTSPLIT_SECRET"))
+                .smtpHost(getenv("COSTSPLIT_SMTP_HOST"))
+                .smtpPort(Integer.parseInt(getenv("COSTSPLIT_SMTP_PORT")))
+                .senderMail(getenv("COSTSPLIT_SENDER_EMAIL"))
+                .senderPassword(getenv("COSTSPLIT_SENDER_PASSWORD"))
+                .port(Integer.parseInt(getenv("COSTSPLIT_PORT")))
+                .isLocal(Boolean.parseBoolean(getenv("COSTSPLIT_LOCAL")))
+                .build()
+                .toApp()
+                .start();
+    }
 
     private static byte[] nextSalt() {
         byte[] buf = new byte[16];
@@ -39,8 +69,36 @@ public class App {
         return factory.generateSecret(spec).getEncoded();
     }
 
-    public static Javalin start() {
-        return Javalin.create()
+    public static void sendMail(String smtpHost, int smtpPort, String sender, String senderPassword, String receiver, String subject, String message) {
+        var email = new SimpleEmail();
+        email.setHostName(smtpHost);
+        email.setSmtpPort(smtpPort);
+        email.setAuthenticator(new DefaultAuthenticator(sender, senderPassword));
+        try {
+            email.setFrom(sender);
+        } catch (EmailException e) {
+            throw new InternalServerErrorResponse("Something went wrong with our email service");
+        }
+        email.setSubject(subject);
+        try {
+            email.addTo(receiver);
+        } catch (EmailException e) {
+            throw new BadRequestResponse("Invalid email");
+        }
+        try {
+            email.setMsg(message);
+        } catch (EmailException e) {
+            throw new InternalServerErrorResponse("Couldn't generate mail message");
+        }
+        try {
+            email.send();
+        } catch (EmailException e) {
+            throw new InternalServerErrorResponse("Couldn't send confirmation mail");
+        }
+    }
+
+    public App start() {
+        javalin = Javalin.create()
                 .beforeMatched(ctx -> {
                     if (ctx.path().startsWith("/auth/"))
                         return;
@@ -60,26 +118,48 @@ public class App {
                 })
                 .post(Request.CreateUser.ENDPOINT, ctx -> {
                     var body = ctx.bodyAsClass(Request.CreateUser.class);
+
+                    // Create JWT data
                     var salt = nextSalt();
                     var saltString = Base64.getEncoder().encodeToString(salt);
                     var hash = Base64.getEncoder().encodeToString(hashPassword(body.password().toCharArray(), salt));
-                    System.out.println("********* Create user *********");
-                    System.out.println("Name    : " + body.name());
-                    System.out.println("Email   : " + body.email());
-                    System.out.println("Salt    : " + saltString);
-                    System.out.println("Password: " + hash);
-                    System.out.println("URL     : https://" + DOMAIN + "/verify?token="
+                    var url = "https://" + config.host + ":" + config.port + "/verify/"
                             + JWT.create()
                             .withClaim("username", body.name())
                             .withClaim("email", body.email())
                             .withClaim("salt", saltString)
                             .withClaim("password", hash)
                             .withExpiresAt(Instant.now().plus(30, ChronoUnit.MINUTES))
-                            .sign(algorithm));
-                }).start(8080);
+                            .sign(algorithm);
+
+                    var subject = "Confirm your CostSplit account";
+                    var message = "Hello " + body.name() + "!\n"
+                            + "We are sending you this message so you can confirm the creation of your account.\n"
+                            + "In order to verify please access the following link:\n"
+                            + url;
+                    sendMail(config.smtpHost, config.smtpPort, config.senderMail, config.senderPassword, body.email(), subject, message);
+                })
+                .start(config.port);
+        return this;
     }
 
-    public static void main(String[] args) {
-        start();
+    public void stop() {
+        javalin.stop();
+    }
+
+    @Builder
+    public record Config(
+            String host,
+            int port,
+            String secret,
+            boolean isLocal,
+            String smtpHost,
+            int smtpPort,
+            String senderMail,
+            String senderPassword
+    ) {
+        App toApp() {
+            return new App(this);
+        }
     }
 }
--- app/src/test/java/io/github/costsplit/app/AppTest.java
+++ app/src/test/java/io/github/costsplit/app/AppTest.java
@@ -1,37 +1,52 @@
 package io.github.costsplit.app;
 
 import io.github.costsplit.api.Request;
-import io.javalin.Javalin;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.jackson.JacksonConverterFactory;
 import retrofit2.http.Body;
 import retrofit2.http.POST;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class AppTest {
+    App app;
+    Retrofit retrofit;
 
-    static Javalin app;
-    static Retrofit retrofit;
+    @BeforeEach
+    void setUp() {
+        app = App.Config.builder()
+                .secret("1234")
+                .host("localhost")
+                .port(8080)
+                .senderMail("sender@test.net")
+                .senderPassword("1234")
+                .smtpHost("localhost")
+                .smtpPort(1)
+                .isLocal(true)
+                .build()
+                .toApp()
+                .start();
 
-    @BeforeAll
-    static void init() {
-        app = App.start();
         retrofit = new Retrofit.Builder()
                 .addConverterFactory(JacksonConverterFactory.create())
-                .baseUrl("http://localhost:" + app.port() + "/")
+                .baseUrl("http://" + app.getConfig().host() + ":" + app.getConfig().port() + "/")
                 .build();
     }
 
+    @AfterEach
+    void tearDown() {
+        app.stop();
+    }
+
     @Test
-    void testCreateUser() {
-        var call = retrofit.create(AppService.class).createUser(new Request.CreateUser("juan", "juan@example.com", "1234"));
+    void createUser() {
+        var call = retrofit.create(AppService.class)
+                .createUser(new Request.CreateUser("receiver", "receiver@test.net", "p455w0rd"));
         var response = assertDoesNotThrow(call::execute);
-        assertTrue(response.isSuccessful());
+        var callWasCorrect = response.isSuccessful() || response.code() == 500;
+        assertTrue(callWasCorrect);
     }
 
     public interface AppService {
